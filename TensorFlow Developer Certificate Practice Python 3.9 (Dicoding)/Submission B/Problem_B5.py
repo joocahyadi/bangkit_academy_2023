f051229a2304df2988e3a2176326e9e4dbc5a5cb
Joshia Cahyadi <70884538+joocahyadi@users.noreply.github.com>
2023-09-30 10:54:06 +0700

    Add files via upload

---
+++ TensorFlow Developer Certificate Practice Python 3.9 (Dicoding)/Submission B/Problem_B5.py
@@ -0,0 +1,97 @@
+# ============================================================================================
+# PROBLEM B5
+#
+# Build and train a neural network model using the Daily Max Temperature.csv dataset.
+# Use MAE as the metrics of your neural network model.
+# We provided code for normalizing the data. Please do not change the code.
+# Do not use lambda layers in your model.
+#
+# The dataset used in this problem is downloaded from https://github.com/jbrownlee/Datasets
+#
+# Desired MAE < 0.2 on the normalized dataset.
+# ============================================================================================
+
+import tensorflow as tf
+import numpy as np
+# import matplotlib.pyplot as plt
+import csv
+import urllib
+
+
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+    series = tf.expand_dims(series, axis=-1)
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    ds = ds.window(window_size + 1, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda w: w.batch(window_size + 1))
+    ds = ds.shuffle(shuffle_buffer)
+    ds = ds.map(lambda w: (w[:-1], w[1:]))
+    return ds.batch(batch_size).prefetch(1)
+
+
+def solution_B5():
+    data_url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/daily-max-temperatures.csv'
+    urllib.request.urlretrieve(data_url, 'daily-max-temperatures.csv')
+
+    time_step = []
+    temps = []
+
+    with open('daily-max-temperatures.csv') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader)
+        step = 0
+        for row in reader:
+            temps.append(float(row[1]))
+            time_step.append(step)
+            step=step + 1
+
+    series= temps
+
+    # Normalization Function. DO NOT CHANGE THIS CODE
+    min=np.min(series)
+    max=np.max(series)
+    series -= min
+    series /= max
+    time=np.array(time_step)
+
+    # DO NOT CHANGE THIS CODE
+    split_time=2500
+
+    x_train = series[:split_time]
+    x_test = series[split_time:]
+    time_train = time[:split_time]
+    time_test = series[split_time:]
+
+    # DO NOT CHANGE THIS CODE
+    window_size=64
+    batch_size=256
+    shuffle_buffer_size=1000
+
+    train_set=windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
+
+    # Define the model architecture
+    model=tf.keras.models.Sequential([
+        tf.keras.layers.LSTM(units=64, return_sequences=True, input_shape=[None, 1]),
+        tf.keras.layers.LSTM(units=32),
+        tf.keras.layers.Dense(units=128, activation='relu'),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Dense(units=64, activation='relu'),
+        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.Dense(units=1)
+    ])
+
+    # Compile the model
+    model.compile(optimizer='sgd', loss='mae', metrics=['mae'])
+
+    # Train the model
+    model.fit(train_set, epochs=50)
+
+    # YOUR CODE HERE
+    return model
+
+
+# The code below is to save your model as a .h5 file.
+# It will be saved automatically in your Submission folder.
+if __name__ == '__main__':
+    # DO NOT CHANGE THIS CODE
+    model=solution_B5()
+    # model.save("model_B5.h5")
